a071ba8f42ee9c0ba27bb64eb92f2f77e819ab14
Olena Poplias <poplyasitasony@gmail.com>
2024-06-06 14:21:55 -0400

    "Sol Pop fixed"

--- app/main.py
+++ app/main.py
@@ -97,22 +97,3 @@ class Battleship:
                             "Ships shouldn't be located "
                             "in the neighboring cells"
                         )
-
-
-battleship = Battleship(
-    ships=[
-        ((0, 0), (0, 3)),
-        ((0, 5), (0, 6)),
-        ((0, 8), (0, 9)),
-        ((2, 0), (4, 0)),
-        ((2, 4), (2, 6)),
-        ((2, 8), (2, 9)),
-        ((9, 9), (9, 9)),
-        ((7, 7), (7, 7)),
-        ((7, 9), (7, 9)),
-        ((9, 7), (9, 7)),
-    ]
-)
-
-
-battleship.print_field()
